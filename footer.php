d092a560c1362e56234d7385bc1b58ed73c64c88
Jesus Vieira <jesusvieiradelima@gmail.com>
2020-05-15 20:40:24 -0300

    Add recurso para personalizar a tag code

--- footer.php
+++ footer.php
@@ -8,6 +8,9 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106445039-1"></script>
     <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-106445039-1');</script>
     <?php endif; ?>
+
+    <script src="<?php echo get_template_directory_uri(); ?>/libs/js/prism.js"></script>
+
     <?php wp_footer(); ?>
 </body>
 
