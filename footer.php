1ff5c4e9f72523f9b21bc59dd2ca702e02a2a1f4
Jesus Vieira <jesusvieiradelima@gmail.com>
2020-05-15 20:02:27 -0300

    add analytics

--- footer.php
+++ footer.php
@@ -3,6 +3,10 @@
             <a href="/">&copy; 2020 Escrever Código</a>
         </span>
     </footer>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106445039-1"></script>
+    <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-106445039-1');</script>
+
     <?php wp_footer(); ?>
 </body>
 
