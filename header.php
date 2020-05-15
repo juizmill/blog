a2a18b6ba4b72223ba97465d0fef5fe24f0da9f7
Jesus Vieira <jesusvieiradelima@gmail.com>
2020-05-15 19:58:03 -0300

    Arrumando fiveicon

--- header.php
+++ header.php
@@ -5,19 +5,19 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#ffffff">
-    <link rel="apple-touch-icon" sizes="57x57" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href=<?php echo get_template_directory_uri(); ?>"/img/faveicon/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/faveicon/favicon-16x16.png">
 
     <?php wp_head(); ?>
 </head>
